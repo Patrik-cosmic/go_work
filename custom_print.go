f5691565e781c020c6adbc1b1c3698a46b762187
Patrik-cosmic <sumande2019@outlook.com>
2021-09-09 12:37:19 +0530

    Add initialization, variable type, user input

---
+++ custom_print.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func print(format string, a ...interface{}) {
+	fmt.Println(a)
+}
--- hello.go
+++ hello.go
@@ -4,4 +4,35 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	/*
+		// Initialization 101
+			//type 1:
+			var name string
+			name = "Suman"
+			fmt.Println(name)
+
+			//type 2:
+			var name = "Suman"
+			fmt.Println(name)
+
+			// another way
+			name := "Suman"
+			fmt.Println(name)
+
+	*/
+
+	// Check the type of the variable/identifier. Printf is same as C
+	name := "Suman"
+	fmt.Printf("The type of variable \"name\" is \"%T\" \n", name)
+
+	// User Input and Output
+
+	var number int
+	fmt.Print("Enter a number: ")
+	fmt.Scanf("%d", &number)
+	fmt.Printf("The number is %d \n", number)
+	// another way
+	fmt.Printf("The number is %v \n", number)
+	print("Hi")
 }
